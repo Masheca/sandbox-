d0efddcc2192f404a6a10cebad8a598ee0037095
Masheka <mashecakoz00@gmail.com>
2024-02-22 22:46:57 +0300

    fake-binary

---
+++ codewars/fake-binary.js
@@ -0,0 +1,14 @@
+// Given a string of digits, you should replace any digit below 5 with '0' and 
+// any digit 5 and above with '1'. Return the resulting string.
+
+
+function fakeBin(x){
+  let result = "";
+   for(let i = 0; i < x.length; i++){
+     if(x[i] >= 5){
+       result += "1";
+     }else{
+       result += "0";
+     }
+   } return result
+ }
