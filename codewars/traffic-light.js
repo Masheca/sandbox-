4baf7f1c325cccef9e5775f2d83894bba60c2bf9
Masheka <mashecakoz00@gmail.com>
2024-02-22 21:15:34 +0300

    Traffic light

---
+++ codewars/traffic-light.js
@@ -0,0 +1,9 @@
+// you're writing code to control your town's traffic lights. You need a function
+//  to handle each change from green, to yellow, to red, and then to green again.
+
+
+function updateLight ( current){ 
+  return current ==="yellow" ? "red" : current ==="green" ? "yellow" :"green";
+
+
+}
