17c052974713a8228ef4a9f91d31c55f7aaa0ca9
Masheka <mashecakoz00@gmail.com>
2024-03-16 17:18:32 +0300

    friend-or-foe

---
+++ codewars/Friend.or.foe.js
@@ -0,0 +1,4 @@
+/*Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours!*/
+const friend = friends => friends.filter(element => element.length === 4)
